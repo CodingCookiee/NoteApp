3ea18e49769047667baa09a24ad100771e439ab3
RK <rzqutubawan@gmail.com>
2024-09-10 16:09:43 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import cssExport from 'vite-plugin-css-export';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    cssExport(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: {
+          title: 'ReactTemplateApp',
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
